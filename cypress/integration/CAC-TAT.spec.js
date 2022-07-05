59e8f80c8595c46f9e91d1859bf29622981f76b1
Felipe Braga <felipe.braga@valtech.com>
2022-07-05 10:11:44 -0300

    test

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -247,7 +247,7 @@ describe('Central de Atendimento ao Cliente TAT', function () {
   it('seleciona um arquivo da pasta fixtures', function(){
     cy.get('input[type="file"][id="file-upload"]')
       .should('not.have.value')
-    //O caminho funciona porque o cypressa leva em consideração o cypress.json
+    //O caminho funcionas porque o cypressa leva em consideração o cypress.json
       .selectFile('./cypress/fixtures/example.json')
       .should(function($input){
         expect($input[0].files[0].name).to.equal('example.json')
